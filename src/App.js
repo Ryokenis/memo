e54bb3d14fd13972b3adaae8de0923b4e453803d
Ryokenis <hazukenis@gmail.com>
2022-08-10 22:11:44 -0400

    Fix App.js and add some styling back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+import Modal from './components/Modal';
 
 function App() {
+  const [post, setPost] = useState({
+    title: '',
+    content: '',
+  });
+
+  const [postArray, setPostArray] = useState([]);
+
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+
+    setPost((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setPostArray((prevValue) => [...prevValue, post]);
+    setPost({
+      title: '',
+      content: '',
+    });
+    toggleModal();
+  };
+
+  const deletePost = (id) => {
+    setPostArray((prevPost) => {
+      return prevPost.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="container">
+        <Navbar toggleModal={toggleModal} />
+        {modal && (
+          <Modal
+            toggleModal={toggleModal}
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+          />
+        )}
+        {postArray.map((post, index) => {
+          return (
+            <Posts
+              key={index}
+              id={index}
+              title={post.title}
+              content={post.content}
+              deletePost={deletePost}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
